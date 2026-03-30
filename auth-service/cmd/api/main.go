4d2b60ab5da31cc8cc90b72861ce29a805976e51
Tobias S <44494591+Tobio89@users.noreply.github.com>
2026-03-30 20:08:46 +0900

    feat(auth): add postgres connection

--- auth-service/cmd/api/main.go
+++ auth-service/cmd/api/main.go
@@ -1,19 +1,40 @@
 package main
 
 import (
+	"database/sql"
+	"discord-profile/auth-service/data"
 	"fmt"
 	"log"
 	"net"
 	"net/rpc"
+	"os"
+	"time"
+
+	_ "github.com/jackc/pgconn"
+	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
 )
 
 const rpcPort = "5001"
+const connectionRetryDelay = 2 * time.Second
+const connectionRetryLimit = 10
 
-type Config struct{}
+var connectionRetryCount int64
+
+type Config struct {
+	Repo data.Repository
+}
 
 func main() {
 
 	app := Config{}
+
+	dbConnection := connectToDB()
+	if dbConnection == nil {
+		log.Panicln("Could not connect to database")
+	}
+
+	app.setupRepo(dbConnection)
 
 	err := rpc.Register(new(RPCServer))
 	if err != nil {
@@ -45,4 +66,54 @@ func (app *Config) rpcListen() error {
 		go rpc.ServeConn(rpcConn)
 	}
 
+}
+
+// Opens connection to DB
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+// Handles retrying DB connection
+func connectToDB() *sql.DB {
+	dsn := os.Getenv("DSN")
+
+	// TODO: handle empty dsn env case
+
+	for {
+		connection, err := openDB(dsn)
+		if err != nil {
+			log.Println("Postgres not yet ready")
+			connectionRetryCount++
+		} else {
+			log.Println("Connected to Postgres")
+			return connection
+		}
+
+		if connectionRetryCount > connectionRetryLimit {
+			log.Println(err)
+			return nil
+		}
+
+		log.Println("Waiting to retry...")
+		time.Sleep(connectionRetryDelay)
+		continue
+	}
+
+}
+
+func (app *Config) setupRepo(conn *sql.DB) {
+	db := data.NewPostgresRepository(conn)
+	app.Repo = db
 }
