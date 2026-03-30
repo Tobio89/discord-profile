273a8aa8593c3bd1df1e9ce8c8ef754e6dd2fde2
Tobias S <44494591+Tobio89@users.noreply.github.com>
2026-03-30 20:28:45 +0900

    feat(auth): improve response

--- auth-service/cmd/api/rpc.go
+++ auth-service/cmd/api/rpc.go
@@ -8,6 +8,12 @@ type RPCPayload struct {
 	User string
 }
 
+type RPCLoginPayload struct {
+	Username string
+	ID       string
+	Token    string
+}
+
 func (r *RPCServer) GetLoginURL(payload RPCPayload, resp *string) error {
 
 	log.Println("auth: received request from: ", payload.User)
@@ -15,4 +21,13 @@ func (r *RPCServer) GetLoginURL(payload RPCPayload, resp *string) error {
 	*resp = "http://localhost:5173/" + payload.User
 
 	return nil
+}
+
+func (r *RPCServer) RequestLogin(payload RPCLoginPayload, resp *string) error {
+	log.Println("auth: received login request for user: ", payload.Username)
+
+	*resp = "http://localhost:5173/" + "?user=" + payload.Username + "&id=" + payload.ID
+
+	return nil
+
 }
